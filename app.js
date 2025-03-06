0a4b4d96a31c21ef7e9849730cb87ff5b3494be3
Samarth Bansal <work.samarthbansal@gmaiil.com>
2025-03-07 03:15:44 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 
-const app = express();
+import { connectToDatabase } from './database/mongodb.js';
 
+const app = express();
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
@@ -17,8 +18,10 @@ app.get('/', (req, res) => {
   res.send({"Title" : 'Hello World!'});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
--- config/env.js
+++ config/env.js
@@ -6,3 +6,4 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 // Export variables with defaults
 export const PORT = process.env.PORT; 
 export const NODE_ENV = process.env.NODE_ENV;
+export const DB_URI = process.env.DB_URI;
