ffb03b1162d0b5d14dcc16c7e377c9c461b5004e
Samarth Bansal <work.samarthbansal@gmaiil.com>
2025-03-08 02:59:20 +0530

    arcjet for rate limiting

--- config/env.js
+++ config/env.js
@@ -9,3 +9,5 @@ export const NODE_ENV = process.env.NODE_ENV;
 export const DB_URI = process.env.DB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+export const ARCJET_KEY = process.env.ARCJET_KEY;
+export const ARCJET_ENV = process.env.ARCJET_ENV;
